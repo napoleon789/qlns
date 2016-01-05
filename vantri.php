aa19e8fdd75d2bcb31e82c4782f21a91cd7ef08f
Hà Minh <hoangminh.ivn@outlook.com>
2016-01-05 14:07:25 +0700

    add

--- vantri.php
+++ vantri.php
@@ -1,3 +1,16 @@
 <?php
-$time = '2015-12-28';
-echo strtotime($time);
+$a = array(
+    "thang" => 10,
+    "nam" => 20,
+
+);
+ $b = array(
+     "thang" => 100,
+     "nam" => 40
+ );
+$c = array(
+    "thang" => 10,
+    "nam" => 200
+);
+$m = array_diff ($a,$b,$c);
+print_r($m);
